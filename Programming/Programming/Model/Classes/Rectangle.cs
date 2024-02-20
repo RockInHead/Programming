f0d3a11c5cba3dddf311217775b797211992d35c
RockInHead <steeltim83@gmail.com>
2024-02-20 14:24:53 +0700

    Добавил свойства и автосвойства для класса Rectangle

--- Programming/Programming/Model/Classes/Rectangle.cs
+++ Programming/Programming/Model/Classes/Rectangle.cs
@@ -3,4 +3,38 @@
     private double _length;
     private double _widtht;
     private string _color;
+
+    public double Length
+    {
+        get
+        {
+            return _length;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Длина положительна!");
+            }
+            _length = value;
+        }
+    }
+
+    public double Widtht
+    {
+        get
+        {
+            return _widtht;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Длина положительна!");
+            }
+            _widtht = value;
+        }
+    }
+
+    public string Color { get; set; }
 }
