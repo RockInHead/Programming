a6dab338eaffc5330eb08bc4be9e01a96fd0a6c1
RockInHead <steeltim83@gmail.com>
2024-02-20 14:28:15 +0700

    Добавил свойства и автосвойства для класса Song

--- Programming/Programming/Model/Classes/Song.cs
+++ Programming/Programming/Model/Classes/Song.cs
@@ -3,4 +3,22 @@
     private string _name;
     private string _author;
     private int _durationsSeconds;
+
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int DurationSeconds
+    {
+        get
+        {
+            return _durationsSeconds;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Положительное значение!");
+            }
+            _durationsSeconds = value;
+        }
+    }
 }
