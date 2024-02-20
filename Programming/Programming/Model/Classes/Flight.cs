3ed6e2da327b0b3c15d6966b0aeeaecb95a02716
RockInHead <steeltim83@gmail.com>
2024-02-20 14:21:03 +0700

    Добавил свойства и автосвойства для класса Flights

--- Programming/Programming/Model/Classes/Flight.cs
+++ Programming/Programming/Model/Classes/Flight.cs
@@ -3,4 +3,22 @@
     private string _departurePoint;
     private string _destinationPoint;
     private int _flightTimeMinutes;
+
+    public string DeparturePoint { get; set; }
+    public string DestinationPoint { get; set; }
+
+    public int FlightTimeMinutes 
+    { 
+        get { 
+            return _flightTimeMinutes;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                throw new ArgumentException("Минуты положительны!");
+            }
+            _flightTimeMinutes = value;
+        }
+    }
 }
