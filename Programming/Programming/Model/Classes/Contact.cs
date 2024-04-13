8ce897956fbb5c149d26d35b4d9ae1720b205c9c
RockInHead <steeltim83@gmail.com>
2024-04-13 19:34:14 +0700

    Сделал комментарии к классу Contact

--- Programming/Programming/Model/Classes/Contact.cs
+++ Programming/Programming/Model/Classes/Contact.cs
@@ -2,15 +2,33 @@
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
+/// <summary>
+/// Хранит данные о контакте: Имя, Фамилию, Телефонный номер, Описание контакта.
+/// </summary>
 public class Contact
 {
     //Поля
+    /// <summary>
+    /// Имя человека - string.
+    /// </summary>
     private string _name;
+    /// <summary>
+    /// Фамилия человека - string.
+    /// </summary>
     private string _surname;
+    /// <summary>
+    /// Телефонный номер - int.
+    /// </summary>
     private int _phoneNumber;
+    /// <summary>
+    /// Описание контакта - string.
+    /// </summary>
     private string _desription;
+
     //Свойства и автосвойства
-    
+    /// <summary>
+    /// Возвращает и задает имя человека. Должно состоять только из букв.
+    /// </summary>
     public string Name
     {
         get
@@ -28,7 +46,9 @@ public class Contact
             
         }
     }
-
+    /// <summary>
+    /// Возвращает и задает фамилию человека. Должно состоять только из букв.
+    /// </summary>
     public string Surname
     {
         get
@@ -46,7 +66,9 @@ public class Contact
 
         }
     }
-
+    /// <summary>
+    /// Возвращает и задает телефонный номер человека. Должно состоять только из положительных чисел.
+    /// </summary>
     public int PhoneNumber
     {
         get
@@ -55,23 +77,32 @@ public class Contact
         }
         set
         {
-            /*  if (value < 0)
-              {
-                  throw new ArgumentException();
-              }*/
             Validator.AssertOnPositiveValue(value);
             _phoneNumber = value;
         }
     }
+    /// <summary>
+    /// Возвращает и задает описание человека.
+    /// </summary>
     public string Desription { get; set; }
 
     //Конструктор
+    /// <summary>
+    /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+    /// </summary>
     public Contact()
     {
         Name = "None";
         PhoneNumber = 0;
         Desription = "None";
     }
+    /// <summary>
+    /// Создаёт экземпляр класса.
+    /// </summary>
+    /// <param name="name">Имя.Должно состоять только из букв.</param>
+    /// <param name="surname">Фамилия.Должно состоять только из букв.</param>
+    /// <param name="phoneNumber">Телефонный номер.Должен состоять только из положительных чисел.</param>
+    /// <param name="description">Описание.Ограничений нет</param>
     public Contact(string name,string surname, int phoneNumber,string description)
     {
         Name = name;
@@ -81,6 +112,12 @@ public class Contact
     }
 
     //Методы
+    /// <summary>
+    /// Проверяет,что строка состоит только из букв.
+    /// </summary>
+    /// <param name="value">Проверяемая строка</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
     private bool AssertStringContainsOnlyLetters(string value)
     {
 
