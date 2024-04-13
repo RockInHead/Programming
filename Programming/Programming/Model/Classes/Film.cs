8cd57f3757b1abfdb8f12e145478b74eba7cbe4b
RockInHead <steeltim83@gmail.com>
2024-04-13 19:55:44 +0700

    Сделал комментарии к классу Film

--- Programming/Programming/Model/Classes/Film.cs
+++ Programming/Programming/Model/Classes/Film.cs
@@ -1,16 +1,39 @@
 ﻿using System.Xml.Linq;
-
+/// <summary>
+/// Хранит данные о фильме: Название,Продолжительность в мин,Год выпуска,Жанр,Рейтинг.
+/// </summary>
 public class Film
 {
     //Поля
+    /// <summary>
+    /// Название фильма.
+    /// </summary>
     private string _name;
+    /// <summary>
+    /// Продолжительность в мин.
+    /// </summary>
     private int _durationMinutes;
+    /// <summary>
+    /// Год выпуска.
+    /// </summary>
     private int _releaseYear;
+    /// <summary>
+    /// Жанр фильма.
+    /// </summary>
     private string _genre;
+    /// <summary>
+    /// Рейтинг фильма.
+    /// </summary>
     private double _rating;
 
     //Свойства и автосвойства
+    /// <summary>
+    /// Возвращает и задает название фильма.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Возвращает и задает продолжительность фильма в мин.Должно быть положительным.
+    /// </summary>
     public int DurationMinutes
     {
         get
@@ -24,6 +47,9 @@ public class Film
             _durationMinutes = value;
         }
     }
+    /// <summary>
+    /// Возвращает и задает год выпуска фильма. Должен быть от 1900 до 2024 включительно.
+    /// </summary>
     public int ReleaseYear
     {
         get
@@ -40,7 +66,13 @@ public class Film
             _releaseYear= value;
         }
     }
+    /// <summary>
+    /// Возвращает и задает жанр фильма.
+    /// </summary>
     public string Genre { get; set; }
+    /// <summary>
+    /// Возвращает и задает рейтинг фильма.Должен быть положительным.
+    /// </summary>
     public double Rating
     {
         get
@@ -60,6 +92,9 @@ public class Film
     }
 
     //Конструктор
+    /// <summary>
+    /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+    /// </summary>
     public Film()
     {
         Name = "None";
@@ -69,6 +104,14 @@ public class Film
         Rating = 0;
 
     }
+    /// <summary>
+    /// Содает экземпляр класса.
+    /// </summary>
+    /// <param name="name">Название.</param>
+    /// <param name="durationMinutes">Продолжительность в мин.Должно быть положительным.</param>
+    /// <param name="releaseYear">Год выпуска фильма.От 1900 до 2024 включительно.</param>
+    /// <param name="genre">Жанр фильма.</param>
+    /// <param name="rating">Рейтинг фильма.Должен быть положительным</param>
     public Film(string name,int durationMinutes,int releaseYear,string genre,double rating)
     {
         Name = name;
