6c6e7f8973f527f7e1295dee1c810347dc50e9b1
RockInHead <steeltim83@gmail.com>
2024-04-13 19:47:14 +0700

    Сделал комментарии к классу Discipline

--- Programming/Programming/Model/Classes/Discipline.cs
+++ Programming/Programming/Model/Classes/Discipline.cs
@@ -1,14 +1,35 @@
 using System.Xml.Linq;
+/// <summary>
+/// Хранит данные о дисциплине:Название,Общее кол-во часов,Оценку,Фамилию учителя.
+/// </summary>
 public class Discipline
 {
     //Поля
+    /// <summary>
+    /// Название дисциплины - string.
+    /// </summary>
     private string _name;
+    /// <summary>
+    /// Общее кол-во часов на дисциплину - int.
+    /// </summary>
     private int _hoursInGeneral;
+    /// <summary>
+    /// Оценка за дисциплину - int.
+    /// </summary>
     private int _assessment;
+    /// <summary>
+    /// Фамилия учителя по дисциплне - string.
+    /// </summary>
     private string _teacherSurname;
 
     //Свойства и автосвойства
+    /// <summary>
+    /// Возвращает и задает название дисциплины.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Возвращает и задает общее кол-во часов на дисциплину.Должно быть положительным числом.
+    /// </summary>
     public int HoursInGeneral
     {
         get
@@ -17,14 +38,14 @@ public class Discipline
         }
         set
         {
-            /*if (value < 0)
-            {
-                throw new ArgumentException("Часы положительны!");
-            }*/
+         
             Validator.AssertOnPositiveValue(value);
             _hoursInGeneral = value;
         }
     }
+    /// <summary>
+    /// Возвращает и задает оценку за дисциплину.Должно быть положительным числом.
+    /// </summary>
     public int Assessment
     {
         get
@@ -41,9 +62,15 @@ public class Discipline
             _assessment = value;
         }
     }
+    /// <summary>
+    /// Возвращает и задает фамилию учителя по дисциплине.
+    /// </summary>
     public string TeacherSurname { get; set; }
-    
+
     //Конструктор
+    /// <summary>
+    /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+    /// </summary>
     public Discipline()
     {
         Name = "None";
@@ -51,6 +78,13 @@ public class Discipline
         Assessment = 0;
         TeacherSurname = "None";
     }
+    /// <summary>
+    /// Создает экземпляр класса.
+    /// </summary>
+    /// <param name="name">Название дисциплины</param>
+    /// <param name="hoursInGeneral">Общее кол-во часов на дисциплину.Должно быть положительным.</param>
+    /// <param name="assessment">Оценка.Должна быть положительной.</param>
+    /// <param name="teacherSurname">Фамилия учителя по дисциплине.</param>
     public Discipline(string name, int hoursInGeneral, int assessment,string teacherSurname)
     {
         Name = name;
