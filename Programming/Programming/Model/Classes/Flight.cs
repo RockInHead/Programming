caf5371426a2f56c13c9fe7bf37c219dc01b5363
RockInHead <steeltim83@gmail.com>
2024-04-13 20:04:17 +0700

    Сделал комментарии к классу Flight

--- Programming/Programming/Model/Classes/Flight.cs
+++ Programming/Programming/Model/Classes/Flight.cs
@@ -1,13 +1,34 @@
-﻿public class Flight
+﻿/// <summary>
+/// Хранит данные о рейсе:Пункт отправления, Пункт прибытия Продолжительность полета в мин.
+/// </summary>
+public class Flight
 {
     //Поля
+    /// <summary>
+    /// Пункт отправления.
+    /// </summary>
     private string _departurePoint;
+    /// <summary>
+    /// Пункт прибытия.
+    /// </summary>
     private string _destinationPoint;
+    /// <summary>
+    /// Продолжительноость полета в мин.
+    /// </summary>
     private int _flightTimeMinutes;
 
     //Свойства  автосвойства
+    /// <summary>
+    /// Возвращает и задает пункт отправления.
+    /// </summary>
     public string DeparturePoint { get; set; }
+    /// <summary>
+    /// Возвращает и задает пункт следования.
+    /// </summary>
     public string DestinationPoint { get; set; }
+    /// <summary>
+    /// Возвращает и задает продолжительность полета в мин. Должно быть положительным.
+    /// </summary>
     public int FlightTimeMinutes 
     { 
         get { 
@@ -15,22 +36,28 @@
         }
         set
         {
-            /*if(value < 0)
-            {
-                throw new ArgumentException("Минуты положительны!");
-            }*/
+            
             Validator.AssertOnPositiveValue(value);
             _flightTimeMinutes = value;
         }
     }
 
     //Конструктор
+    /// <summary>
+    /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+    /// </summary>
     public Flight()
     {
         DeparturePoint = "None";
         DestinationPoint = "None";
         FlightTimeMinutes = 0;
     }
+    /// <summary>
+    /// Создает экземпляр класса
+    /// </summary>
+    /// <param name="departurePoint">Пункт отправления.</param>
+    /// <param name="destinationPoint">Пункт следования.</param>
+    /// <param name="flightTimeMinutes">Прдолжительность полета в мин. Должна быть положительной.</param>
     public Flight(string departurePoint,string destinationPoint,int flightTimeMinutes)
     {
         DeparturePoint = departurePoint;
