1f93e07fe3cc77ce09b8365145676f5121f897eb
RockInHead <steeltim83@gmail.com>
2024-04-15 14:22:14 +0700

    Сделал комментарии к классу CollisionManager

--- Programming/Programming/Model/Classes/Geometry/CollisionManager.cs
+++ Programming/Programming/Model/Classes/Geometry/CollisionManager.cs
@@ -1,5 +1,14 @@
-﻿static public class CollisionManager
+﻿/// <summary>
+/// Осуществляет проверку пересечния геометрических фигур.
+/// </summary>
+static public class CollisionManager
 {
+    /// <summary>
+    /// Проверяет пересекаются ли 2 прямоугольника типа данных Rectangle.
+    /// </summary>
+    /// <param name="rectangle1">Первый прямуогольник</param>
+    /// <param name="rectangle2">Второй прямоугольник</param>
+    /// <returns></returns>
     static public bool isCollision(Rectangle rectangle1, Rectangle rectangle2)
     {
         double differenceX = Math.Abs(rectangle1.Center.CoordinateX - rectangle2.Center.CoordinateX);
@@ -9,6 +18,12 @@
 
         return differenceX < halfLatitude && differenceY < halfLength;
     }
+    /// <summary>
+    /// Проверяет пересекаются ли 2 кольца типа данных Ring.
+    /// </summary>
+    /// <param name="rectangle1">Первое кольцо.</param>
+    /// <param name="rectangle2">Второе кольцо.</param>
+    /// <returns></returns>
     static public bool isCollision(Ring ring1,Ring ring2)
     {
         double hypotenuse = Math.Sqrt(Math.Pow(Math.Abs(ring1.Center.CoordinateX - ring2.Center.CoordinateX), 2) + Math.Pow(Math.Abs(ring1.Center.CoordinateY - ring2.Center.CoordinateY), 2));
