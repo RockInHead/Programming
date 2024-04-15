8da9ebc28139116924263a2806c908501963ceab
RockInHead <steeltim83@gmail.com>
2024-04-15 16:46:12 +0700

    Сделал комментарии к WeekdayParsingControl

--- Programming/Programming/View/Panels/WeekdayParsingControl.cs
+++ Programming/Programming/View/Panels/WeekdayParsingControl.cs
@@ -10,13 +10,20 @@ using System.Windows.Forms;
 
 namespace Programming.View.Panels
 {
+    /// <summary>
+    /// Осущесвтялет парсинг дня недели.
+    /// </summary>
     public partial class WeekdayParsingControl : UserControl
     {
         public WeekdayParsingControl()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Првоеряет, есть ли указанная строка в днях недели. Если да, то выводит его номер в неделе, если нет ,то информирует пользователя о том,что строки нет в днях недели.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ParseButton_Click(object sender, EventArgs e)
         {
             string ParsingText = ParsingTextBox.Text;
