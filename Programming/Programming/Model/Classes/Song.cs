338feaeda8b181764448cc3efb6f7633f9a2dca4
RockInHead <steeltim83@gmail.com>
2024-04-15 10:12:12 +0700

    Проежуточный коммит: НЕ доделал класс Song

--- Programming/Programming/Model/Classes/Song.cs
+++ Programming/Programming/Model/Classes/Song.cs
@@ -1,14 +1,34 @@
 ﻿using System.Xml.Linq;
-
+/// <summary>
+/// Хранит данные о песне: Название,Автора,Продолжительность в сек.
+/// </summary>
 public class Song
 {
     //Поля
+    /// <summary>
+    /// Название песни.
+    /// </summary>
     private string _name;
+    /// <summary>
+    /// Автор песни.
+    /// </summary>
     private string _author;
+    /// <summary>
+    /// Продолжительность в сек.
+    /// </summary>
     private int _durationsSeconds;
     //Свойства и автосвойства
+    /// <summary>
+    /// Возваращает и задает название песни.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Возвращает и задает автора песни.
+    /// </summary>
     public string Author { get; set; }
+    /// <summary>
+    /// Возвращает и задает продолжительность пенсни в сек. Должна быть положительной.
+    /// </summary>
     public int DurationSeconds
     {
         get
@@ -26,12 +46,21 @@ public class Song
         }
     }
     //Конструктор
+    /// <summary>
+    /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+    /// </summary>
     public Song()
     {
         Name = "None";
         Author = "None";
         DurationSeconds = 0;
     }
+    /// <summary>
+    /// Создает экземпляр класса.
+    /// </summary>
+    /// <param name="name">Название песни.</param>
+    /// <param name="author"></param>
+    /// <param name="durationSeconds"></param>
     public Song(string name, string author,int durationSeconds)
     {
         Name = name;
