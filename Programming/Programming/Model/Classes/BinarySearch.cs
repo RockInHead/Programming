dd9683e15fe166aae7996fa68633347b60443a71
RockInHead <steeltim83@gmail.com>
2024-04-29 22:13:02 +0700

    Начачата 8 лаба

---
+++ Programming/Programming/Model/Classes/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace Programming.Model.Classes
+{
+    internal class BinarySearch
+    {
+        public int BinarySearchFunction(int[] array, int x)
+        {
+            int L = 0;
+            int R = array.Length - 1;
+
+            while (L <= R)
+            {
+                int Mid = (L + R) / 2;
+                if (array[Mid] < x)
+                {
+                    L = Mid + 1;
+                }
+                else if (array[Mid] > x)
+                {
+                    R = Mid - 1;
+                }
+                else
+                {
+                    return Mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
