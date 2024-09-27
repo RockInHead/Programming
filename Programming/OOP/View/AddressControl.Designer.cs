97aadb85a9e2505658fbd69d3d0ccab96be8d2c5
RockInHead <steeltim83@gmail.com>
2024-09-28 01:30:37 +0700

    Сверстал еще один пользовательский элемент управления AddressControl.

---
+++ Programming/OOP/View/AddressControl.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace OOP.View
+{
+    partial class AddressControl
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            PostIndexTextBox = new TextBox();
+            CountryTextBox = new TextBox();
+            label3 = new Label();
+            CityTextBox = new TextBox();
+            label4 = new Label();
+            StreetTextBox = new TextBox();
+            label5 = new Label();
+            BuildingTextBox = new TextBox();
+            label6 = new Label();
+            ApartmentTextBox = new TextBox();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Delivery Address";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(0, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Post Index:";
+            // 
+            // PostIndexTextBox
+            // 
+            PostIndexTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            PostIndexTextBox.Location = new Point(114, 54);
+            PostIndexTextBox.Name = "PostIndexTextBox";
+            PostIndexTextBox.Size = new Size(180, 34);
+            PostIndexTextBox.TabIndex = 2;
+            // 
+            // CountryTextBox
+            // 
+            CountryTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            CountryTextBox.Location = new Point(114, 115);
+            CountryTextBox.Name = "CountryTextBox";
+            CountryTextBox.Size = new Size(278, 34);
+            CountryTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(0, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Country:";
+            // 
+            // CityTextBox
+            // 
+            CityTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            CityTextBox.Location = new Point(471, 115);
+            CityTextBox.Name = "CityTextBox";
+            CityTextBox.Size = new Size(278, 34);
+            CityTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(415, 115);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 28);
+            label4.TabIndex = 5;
+            label4.Text = "City:";
+            // 
+            // StreetTextBox
+            // 
+            StreetTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            StreetTextBox.Location = new Point(114, 173);
+            StreetTextBox.Name = "StreetTextBox";
+            StreetTextBox.Size = new Size(635, 34);
+            StreetTextBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(3, 173);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 28);
+            label5.TabIndex = 7;
+            label5.Text = "Street:";
+            // 
+            // BuildingTextBox
+            // 
+            BuildingTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            BuildingTextBox.Location = new Point(117, 237);
+            BuildingTextBox.Name = "BuildingTextBox";
+            BuildingTextBox.Size = new Size(122, 34);
+            BuildingTextBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label6.Location = new Point(3, 237);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 28);
+            label6.TabIndex = 9;
+            label6.Text = "Building:";
+            // 
+            // ApartmentTextBox
+            // 
+            ApartmentTextBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ApartmentTextBox.Location = new Point(396, 237);
+            ApartmentTextBox.Name = "ApartmentTextBox";
+            ApartmentTextBox.Size = new Size(122, 34);
+            ApartmentTextBox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label7.Location = new Point(280, 237);
+            label7.Name = "label7";
+            label7.Size = new Size(110, 28);
+            label7.TabIndex = 11;
+            label7.Text = "Apartment:";
+            // 
+            // AddressControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(ApartmentTextBox);
+            Controls.Add(label7);
+            Controls.Add(BuildingTextBox);
+            Controls.Add(label6);
+            Controls.Add(StreetTextBox);
+            Controls.Add(label5);
+            Controls.Add(CityTextBox);
+            Controls.Add(label4);
+            Controls.Add(CountryTextBox);
+            Controls.Add(label3);
+            Controls.Add(PostIndexTextBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AddressControl";
+            Size = new Size(763, 298);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox PostIndexTextBox;
+        private TextBox CountryTextBox;
+        private Label label3;
+        private TextBox CityTextBox;
+        private Label label4;
+        private TextBox StreetTextBox;
+        private Label label5;
+        private TextBox BuildingTextBox;
+        private Label label6;
+        private TextBox ApartmentTextBox;
+        private Label label7;
+    }
+}
