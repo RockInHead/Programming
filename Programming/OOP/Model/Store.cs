283ad6cffac7fc767ac0e54cc43bc09809f6207d
RockInHead <steeltim83@gmail.com>
2024-10-08 22:24:25 +0700

    Создал класс Store, пока без проверки правильности реализации

---
+++ Programming/OOP/Model/Store.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    /// <summary>
+    /// Хранит данные о списке товаров и списке покупателей.
+    /// </summary>
+    internal class Store
+    {
+        /// <summary>
+        /// Список товаров.
+        /// </summary>
+        private List<Item> _items;
+        /// <summary>
+        /// Список покупателей.
+        /// </summary>
+        private List<Customer> _customers;
+
+        /// <summary>
+        /// Возвращает и задает список товаров.
+        /// </summary>
+        public List<Item> Items { get; set; }
+        /// <summary>
+        /// Возвращает и задает список покупателей.
+        /// </summary>
+        public List<Customer> Customerss { get; set; }
+
+        public Store()
+        {
+            Items = new List<Item>();
+            Customerss = new List<Customer>();
+        }
+
+
+    }
+}
