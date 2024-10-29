ff045dac1179dde48adc146af861aefdf912025b
RockInHead <steeltim83@gmail.com>
2024-10-29 21:23:54 +0700

    Создал отнаследованный класс PriorityOrder: Order и специальное перечисление к нему, которое оперделяет для его поля промежутки доставки заказа. ЕЩЕ НЕ ПРОВЕРЯЛ ПРАВИЛЬНОСТЬ

---
+++ Programming/OOP/Model/PriorityOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    internal class PriorityOrder : Order
+    {
+        public DateTime DeliveryDateTime {  get; set; }
+        public DeliveryTimeRange DeliveryTimeRange { get; set;}
+
+        public PriorityOrder() { }
+
+        public PriorityOrder(OrderStatus status, string dateOfCreation, List<Item> items, Address address, DateTime deliveryDateTime, DeliveryTimeRange deliveryTimeRange)
+            : base(status,dateOfCreation,items, address)
+        {
+            DeliveryDateTime = deliveryDateTime;
+            DeliveryTimeRange = deliveryTimeRange;
+        }
+
+    }
+ }
+
