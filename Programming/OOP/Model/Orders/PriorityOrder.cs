694320cbf772c669a2a3866a50da85f92c61592f
RockInHead <steeltim83@gmail.com>
2025-01-13 14:49:05 +0700

    Поправил PriorityOrder.

--- Programming/OOP/Model/Orders/PriorityOrder.cs
+++ Programming/OOP/Model/Orders/PriorityOrder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP.Model.Orders
+﻿namespace OOP.Model.Orders
 {
     /// <summary>
     /// Хранит данные о приоритетном заказе: Дата доставки заказа, время доставки заказа.
