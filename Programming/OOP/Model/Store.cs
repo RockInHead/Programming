c58e1dbb0ebc5f9e937e3645ca9636149ef74f94
RockInHead <steeltim83@gmail.com>
2025-01-13 17:10:05 +0700

    Поправил Store.

--- Programming/OOP/Model/Store.cs
+++ Programming/OOP/Model/Store.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP.Model
+﻿namespace OOP.Model
 {
     /// <summary>
     /// Хранит данные о списке товаров и списке покупателей.
@@ -15,6 +9,7 @@ namespace OOP.Model
         /// Список товаров.
         /// </summary>
         private List<Item> _items;
+
         /// <summary>
         /// Список покупателей.
         /// </summary>
@@ -24,20 +19,20 @@ namespace OOP.Model
         /// Возвращает и задает список товаров.
         /// </summary>
         public List<Item> Items { get; set; }
+
         /// <summary>
         /// Возвращает и задает список покупателей.
         /// </summary>
         public List<Customer> Customers { get; set; }
+
         /// <summary>
-        /// Создает пустой/начальный экземпляр класса.Всем полям присваивается значение по умолчанию.
+        /// Инициализирует новый экземпляр класса <see cref="Store"/> 
+        /// с пустыми списками товаров и клиентов.
         /// </summary>
         public Store()
         {
             Items = new List<Item>();
             Customers = new List<Customer>();
-            
         }
-
-
     }
 }
