be9193f30d4a5b15269fe166b10a6f2c0032cd78
RockInHead <steeltim83@gmail.com>
2025-01-13 16:52:14 +0700

    Поправил IDiscount.

--- Programming/OOP/Model/IDiscount.cs
+++ Programming/OOP/Model/IDiscount.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP.Model
+﻿namespace OOP.Model
 {
     /// <summary>
     /// Интерфейс для скидок. 
@@ -12,9 +6,29 @@ namespace OOP.Model
     /// </summary>
     public interface IDiscount
     {
+        /// <summary>
+        /// Получает информацию о скидке.
+        /// </summary>
         string Info { get; }
+
+        /// <summary>
+        /// Вычисляет сумму скидки для списка предметов.
+        /// </summary>
+        /// <param name="items">Список предметов.</param>
+        /// <returns>Сумма скидки.</returns>
         double Calculate(List<Item> items);
+
+        /// <summary>
+        /// Применяет скидку к списку предметов и возвращает общую сумму после применения скидки.
+        /// </summary>
+        /// <param name="items">Список предметов.</param>
+        /// <returns>Общая сумма после применения скидки.</returns>
         double Apply(List<Item> items);
+
+        /// <summary>
+        /// Обновляет информацию о скидке на основе переданного списка предметов.
+        /// </summary>
+        /// <param name="items">Список предметов.</param>
         void Update(List<Item> items);
     }
 }
