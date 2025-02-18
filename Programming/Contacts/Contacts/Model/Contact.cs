f5bcb7695172b6d819ef3c8e404decf451fd2656
RockInHead <steeltim83@gmail.com>
2025-02-18 14:44:31 +0700

    Создал 2 папки: Model и ViewModel. В папке Model создал класс Contact, хранящий имя, номер телефон и почту контакта. С автосвойствами и конструктором. Начальный вариант, в дальнейшем класс будет доработан.

---
+++ Programming/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Класс контакта пользователя, хранящий имя, номер телефон и почту контакта.
+/// </summary>
+public class Contact
+{
+    /// <summary>
+    /// Задает и возвращет имя контакта.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Задает и возвращает номер телефона контакта.
+    /// </summary>
+    public string PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Задает и возвращает почту контакта.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Конструктор Contact c параметрами.
+    /// Инициализиурет новый класс с указанными значениями.
+    /// </summary>
+    /// <param name="name">Имя контакта</param>
+    /// <param name="phoneNumber">Номер телефона</param>
+    /// <param name="email">Почта контакта</param>
+    public Contact(string name, string phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+}
+
